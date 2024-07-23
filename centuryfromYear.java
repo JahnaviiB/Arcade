1e64e57a722ce006da0b279d34657ea92be2e858
Jahnavi <jahnavi.b2@gmail.com>
2024-07-22 22:29:33 -0400

    Adding file

--- centuryfromYear.java
+++ centuryfromYear.java
@@ -1,2 +1,10 @@
 public class centuryfromYear {
+    public static int solution(int year){
+        int century=  (year + 99) / 100;
+        return century;
+    }
+    public static void main(String[] args){
+        int result = solution(100);
+        System.out.println(result);
+    }
 }
