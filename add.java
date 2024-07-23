1b285afc3c5c7ee7ceae76dedb51554d21bcef61
Jahnavi <jahnavi.b2@gmail.com>
2024-07-22 22:22:18 -0400

    Adding file

--- add.java
+++ add.java
@@ -1,2 +1,10 @@
 public class add {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int result = add(3,2);
+        System.out.println("The sum is : " + result);
+
+    }
 }
